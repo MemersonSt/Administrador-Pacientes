b22ecff245745305d57fbc5d2faf71e24a3d8222
MemersonSt <emerson.cajape@tecsu.edu.ec>
2023-09-01 16:55:31 -0500

    commit -m "completado"

--- src/Components/ListadoPaciente.jsx
+++ src/Components/ListadoPaciente.jsx
@@ -1,11 +1,11 @@
 import Pacientes from "./Pacientes";
 
-const ListadoPaciente = ({pacientes, setPaciente}) => {
+const ListadoPaciente = ({pacientes, setPaciente, eliminarPaciente}) => {
     return(
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-auto">
             {pacientes && pacientes.length ? (
                 <>
-                    <h2 className="font-black text-3xl text-center mt-6">Listado de Pacientes</h2>
+                    <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
                     <p className="text-xl mt-5 mb-5 text-center">
                         Administras tus {' '}
                         <span className="font-bold text-indigo-600">Pacientes y Citas</span>
@@ -15,10 +15,11 @@ const ListadoPaciente = ({pacientes, setPaciente}) => {
                             key={paciente.id}
                             paciente={paciente}
                             setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
                         />
                     ))}
                 </>
-            ):(
+            ) : (
                 <div className="h-[60%] w-auto flex flex-col text-center justify-center mt-6 mb-4 p-2">
                     <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
                     <p className="text-xl mt-5 mb-5 text-center">
@@ -27,8 +28,6 @@ const ListadoPaciente = ({pacientes, setPaciente}) => {
                     </p>
                 </div>
             )}
-
-            
         </div>
     )
 }
